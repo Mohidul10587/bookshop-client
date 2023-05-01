f3969120675e9ce5d9178a34d91c0d118741cc06
mohid <mohid10587@gmail.com>
2023-05-01 17:04:34 +0600

    update with jwt token

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,15 +4,19 @@ import url from '@/components/url';
 import { ThemeContext } from './_app';
 import jwt_decode from 'jwt-decode';
 import Link from 'next/link';
+import { FiMail, FiLock, FiUserPlus, FiAlertCircle, FiHelpCircle, FiLoader } from 'react-icons/fi';
+
 const Login = () => {
   const value = useContext(ThemeContext);
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
       const response = await fetch(`${url}/login`, {
         method: 'POST',
@@ -24,12 +28,14 @@ const Login = () => {
       const data = await response.json();
       if (!response.ok) {
         setError(data.message);
+        setIsLoading(false);
         return;
       }
       localStorage.setItem('token', data.token);
       router.push('/');
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
 
     const fetchCartProducts = async () => {
@@ -37,7 +43,6 @@ const Login = () => {
         const token = localStorage.getItem('token');
         const decodedToken = jwt_decode(token);
         if (!decodedToken) {
-
           return;
         }
 
@@ -47,7 +52,6 @@ const Login = () => {
           },
         });
         if (!res.ok) {
-
           return;
         }
         const { email } = await res.json();
@@ -55,44 +59,64 @@ const Login = () => {
         const data = await fetch(`${url}/users/${email}`).then((res) => res.json());
 
         if (data.role) {
-
           value.setAdmin(true);
-
         } else {
           value.setAdmin(false);
         }
       } catch (err) {
         console.error(err);
-
       }
     };
     await fetchCartProducts();
-
   };
 
   return (
-    <div className='min-h-screen pt-24 md:px-10 px-2'>
-      <div className='flex justify-center'>
-        <form className='border border-black md:w-1/2 p-10' onSubmit={handleLogin}>
-          <p className='text-center text-3xl'>Login</p>
-          {error && <p className='text-red-500'>{error}</p>}
-          <p>Email</p>
-          <input className='border border-black p-2 rounded-xl w-full' type='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <p>Password</p>
-          <input className='border border-black p-2 rounded-xl w-full' type='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-          <br />
-          <input className='border border-black p-2 rounded-xl mt-4 w-full' type='submit' value='Login' />
-        </form>
-
-      </div>
-      <div className='md:flex justify-center'>
-        <div className='md:w-1/2 flex justify-between'>
-          <Link href='forgotPassword'>Forget password ?</Link>
-          <Link href='signUp'>Create account</Link>
-
+   <div className='min-h-screen pt-24'>
+     <div className='flex justify-center '>
+      <form className='border border-gray-400 bg-violet-400 rounded md:w-1/2 p-10' onSubmit={handleLogin}>
+        <div className='text-center text-3xl mb-4'>Login</div>
+        {error && (
+          <div className='flex items-center space-x-2 mt-4'>
+            <FiAlertCircle size={20} />
+            <div className='text-red-500'>{error}</div>
+          </div>
+        )}
+        <div className='relative'>
+          <FiMail className='absolute top-3 left-3 text-gray-400' />
+          <input className=' p-2 rounded-xl w-full pl-10' type='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
-      </div>
+        <div className='relative mt-4'>
+          <FiLock className='absolute top-3 left-3 text-gray-400' />
+          <input className=' p-2 rounded-xl w-full pl-10' type='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <br />
+        <button className=' p-2  bg-violet-600 hover:bg-violet-700 text-white rounded-xl mt-4 w-full flex items-center justify-center'>
+          {isLoading ? (
+            <div className='flex gap-1 items-center'> <FiLoader className='animate-spin mr-2' /> <span>Loading...</span></div>
+          ) : (
+            'Login'
+          )}
+        </button>
+        <div className='mt-4'>
+          <div className='flex justify-between'>
+            <Link href='forgotPassword'>
+              <div className='text-red-800 hover:text-red-600 flex gap-1 items-center'>
+                <FiHelpCircle className='mr-2' />
+                <p> Forget password?</p>
+              </div>
+            </Link>
+            <Link href='signUp'>
+              <div className='text-green-800 hover:text-green-600 flex gap-1 items-center'>
+                <FiUserPlus className='mr-2' />
+                <p>Create account</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </form>
     </div>
+   </div>
+
   );
 };
 
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import { FiUser, FiMail, FiLock, FiLoader } from 'react-icons/fi';
 import url from '@/components/url';
 
 const Signup = () => {
@@ -11,38 +12,123 @@ const Signup = () => {
     password: ''
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
   const handleInputChange = (event) => {
     setUserData({ ...userData, [event.target.name]: event.target.value });
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch(`${url}/signup`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(userData)
-    });
-    const { token } = await response.json();
-    localStorage.setItem('token', token);
-    router.push('/');
+    setIsLoading(true);
+    setError('');
+
+    try {
+      const response = await fetch(`${url}/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      const { token } = await response.json();
+      localStorage.setItem('token', token);
+      router.push('/');
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <div className='min-h-screen pt-24 md:px-10 px-2'>
-        <div className='flex justify-center'>
-     <form onSubmit={handleFormSubmit} className='border border-black md:w-1/2 p-10'>
-        <p className='text-center text-3xl'>Sign Up</p>
-        <p>Name</p>
-        <input name='name' value={userData.name} onChange={handleInputChange} className='border border-black p-2 rounded-xl w-full' type='text' />
-        <p>Email</p>
-        <input name='email' value={userData.email} onChange={handleInputChange} className='border border-black p-2 rounded-xl w-full' type='email' />
-        <p>Password</p>
-        <input name='password' value={userData.password} onChange={handleInputChange} className='border border-black p-2 rounded-xl w-full' type='password' />
-        <br />
-        <input className='border border-black p-2 rounded-xl mt-4 w-full' type='submit' value='Sign Up' />
-      </form>
-     </div>
-    </div>
+    <div className='min-h-screen pt-24'>
+    <div className='flex justify-center '>
+    <form onSubmit={handleFormSubmit} className="border bg-violet-400 rounded border-gray-400 md:w-1/2 p-10">
+          <p className="text-center text-3xl  mb-4">Sign Up</p>
+          {error && (
+            <div className="flex items-center space-x-2 mb-4">
+              <FiAlertCircle className="text-red-500" size={20} />
+              <p className="text-red-500">{error}</p>
+            </div>
+          )}
+          <div className="mb-4">
+            <label htmlFor="name" className="sr-only">
+              Name
+            </label>
+            <div className="relative">
+              <FiUser className="absolute top-3 left-3 text-gray-400" />
+              <input
+                id="name"
+                name="name"
+                value={userData.name}
+                onChange={handleInputChange}
+                className="border border-gray-400 p-2 rounded-xl w-full pl-10"
+                type="text"
+                placeholder="Name"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <div className="relative">
+              <FiMail className="absolute top-3 left-3 text-gray-400" />
+              <input
+                id="email"
+                name="email"
+                value={userData.email}
+                onChange={handleInputChange}
+                className="border border-gray-400 p-2 rounded-xl w-full pl-10"
+                type="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <div className="relative">
+              <FiLock className="absolute top-3 left-3 text-gray-400" />
+              <input
+                id="password"
+                name="password"
+                value={userData.password}
+                onChange={handleInputChange}
+                className="border border-gray-400 p-2 rounded-xl w-full pl-10"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-violet-600 text-white py-2 px-4 rounded-lg w-full transition duration-300 ease-in-out hover:bg-violet-700"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <div className="flex items-center justify-center">
+                <FiLoader className="animate-spin mr-2" />
+                <span>Loading...</span>
+              </div>
+            ) : (
+              'Sign Up'
+            )}
+          </button>
+        </form>
+   </div>
+  </div>
+
+
   );
 };
 
