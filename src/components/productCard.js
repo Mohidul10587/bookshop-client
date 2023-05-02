3a712ce0cdd4aebfa6c28fb3d00c0236140bc17c
mohid <mohid10587@gmail.com>
2023-05-02 14:54:55 +0600

    ok

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ProductCard = ({ p }) => {
     return (
         <div>
-            <div className='border m-4 border-black md:w-80 w-72 rounded-lg overflow-hidden' >
+            <div className='border-t m-4 border-violet-400 shadow-lg shadow-violet-900 md:w-80 w-72 rounded-lg overflow-hidden hover:scale-110 duration-700' >
                 <img className='w-72 md:w-80 md:h-80 h-72 rounded-t-lg border border-b-black' src={p.img} alt={p.name} />
                 <div className='pl-2 my-2 '>
                     <h2 className='font-bold'>{p.name}</h2>
