f8c1e219e7143117bb516bfa82012d66fe46b6c1
mohid <mohid10587@gmail.com>
2023-05-02 13:25:15 +0600

    Updating..

--- src/pages/productDetails/[productDetails].js
+++ src/pages/productDetails/[productDetails].js
@@ -89,9 +89,9 @@ const ProductDetails = () => {
 
 
   return (
-    <div className='min-h-screen pt-24 px-6'>
+    <div className='min-h-screen pt-24 '>
       <h2 className='text-3xl text-center md:mt-10 mt-4 md:mb-24 mb-10'>{product.name}</h2>
-      <div className='flex justify-center'>
+      <div className='flex justify-center px-6'>
         <div className='md:flex justify-center items-center md:px-10 mb-10'>
 
           <img className='w-[320px] h-[320px] border border-black p-2' src={product.img} alt="" />
